aafa5dfcec83867ec333c6a1b4b9cf6c2bb6db3e
ItaloF532 <italo.francisco@startse.com>
2023-09-26 20:38:27 -0300

    feat: create storage class

---
+++ storage/index.js
@@ -0,0 +1,17 @@
+class Storage {
+  _storage = sessionStorage;
+
+  save(key, data) {
+    this._storage.setItem(key, JSON.stringify(data));
+  }
+
+  delete(key) {
+    this._storage.removeItem(key);
+  }
+  
+  recover(key) {
+    return this._storage.getItem(key);
+  }
+}
+
+export default Storage;
