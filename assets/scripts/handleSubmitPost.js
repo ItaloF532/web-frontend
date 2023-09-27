821c6dc285f24b558764372cf1c9ee39de749a7c
ItaloF532 <italo.francisco@startse.com>
2023-09-27 20:50:12 -0300

    chore: remove unnecessary console log

--- assets/scripts/handleSubmitPost.js
+++ assets/scripts/handleSubmitPost.js
@@ -25,7 +25,6 @@ export function handleSubmitPost() {
       errorMessages.appendChild(errorElement);
     } else {
       updatePostModalDisplay("none");
-      console.log(postController.getPosts());
       postForm.reset();
     }
   });
