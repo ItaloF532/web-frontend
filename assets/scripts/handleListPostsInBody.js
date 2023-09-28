4759b4939da9e3d8f6e04d8c7537b7177e2be959
ItaloF532 <italo.francisco@startse.com>
2023-09-28 14:13:03 -0300

    feat: create script to render post list on screen

---
+++ assets/scripts/handleListPostsInBody.js
@@ -0,0 +1,56 @@
+import Post from "../../posts";
+
+const postController = Post.getInstance();
+const postList = document.getElementById("post-list");
+
+function updatePostList() {
+  let newPostList = postController.getPosts();
+  const lastPost = newPostList[newPostList.length - 1];
+  console.log("lastPost", lastPost);
+
+  postList.innerHTML += `
+      <div id="post-${newPostList.length - 1}" class="post">
+        <h2> ${lastPost?.title ?? "Error on get title"} </h2> 
+        <span> ${lastPost?.date ?? "Error o get date"} </span>
+        <p> ${lastPost?.desc ?? "Error on get description"} </p>
+      </div>
+    `;
+}
+
+function handleCreatePost() {
+  postController.subscribe("created-post", (_) => {
+    if (
+      postController.searchedTitle?.trim() === "" &&
+      postController.searchedDate?.trim() === ""
+    ) {
+      updatePostList();
+    }
+  });
+}
+
+function renderAllPosts() {
+  const allPosts = postController.getPosts();
+  let generatedHTML = "";
+
+  allPosts.forEach((post, index) => {
+    if (post) {
+      generatedHTML += `
+        <div id="post-${index}" class="post">
+          <h2> ${post?.title ?? "Error on get title"} </h2> 
+          <span> ${post?.date ?? "Error o get date"} </span>
+          <p> ${post?.desc ?? "Error on get description"} </p>
+        </div>
+      `;
+    }
+  });
+
+  postList.innerHTML = generatedHTML;
+}
+
+function handleListPostsInBody() {
+  handleCreatePost();
+  renderAllPosts();
+}
+
+// Render posts on html
+export default handleListPostsInBody();
