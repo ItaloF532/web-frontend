b4b06d1b5d09e5d5d3feb296bc2dd71299018dc6
ItaloF532 <italo.francisco@startse.com>
2023-09-28 20:36:00 -0300

    feat: add delete button in post

--- assets/scripts/generateHtmlForPost.js
+++ assets/scripts/generateHtmlForPost.js
@@ -1,6 +1,7 @@
 function generateHtmlForPost(post, index) {
   return `
     <div id="post-${index}" class="post">
+      <img id="delete-post-button-${index}" src="./assets/svg/trash.svg" width="20" height="20" style="width: 100%; position: absolute; display: flex; align-items: flex-end; justify-content: flex-end;">
       <h2> ${post?.title ?? "Error on get title"} </h2> 
       <span> ${post?.date ?? "Error o get date"} </span>
       <p> ${post?.desc ?? "Error on get description"} </p>
