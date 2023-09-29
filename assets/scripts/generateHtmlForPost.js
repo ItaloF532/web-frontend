2685960ed2f48dba6d6871fec6a1d7dbe7e9f32f
ItaloF532 <italo.francisco@startse.com>
2023-09-29 09:03:01 -0300

    refactor: adjust date style

--- assets/scripts/generateHtmlForPost.js
+++ assets/scripts/generateHtmlForPost.js
@@ -1,9 +1,21 @@
+function formatDate(date) {
+  if (!date) return null;
+  const newDate = new Date(date);
+  const month = newDate.toLocaleString("default", { month: "long" });
+  const day = newDate.getDate();
+  const year = newDate.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
+
 function generateHtmlForPost(post, index) {
+  const date = formatDate(post.date);
+
   return `
     <div id="post-${index}" class="post">
       <img id="delete-post-button-${index}" src="./assets/svg/trash.svg" width="20" height="20" style="z-index: 2; width: 200%; display: flex; align-items: flex-end; justify-content: flex-end;">
       <h2> ${post?.title ?? "Error on get title"} </h2> 
-      <span> ${post?.date ?? "Error o get date"} </span>
+      <span> ${date ?? "Error o get date"} </span>
       <p> ${post?.desc ?? "Error on get description"} </p>
     </div>
   `;
