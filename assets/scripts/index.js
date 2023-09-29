5ba08f5888223d94862ad52afe305edc10e67d62
ItaloF532 <italo.francisco@startse.com>
2023-09-29 08:55:25 -0300

    fix: adjust imports

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,10 @@
-import toggleMenu from "./assets/scripts/toggleMenu.js";
-import handlePostModal from "./assets/scripts/handlePostModal.js";
-import handlePostSearch from "./assets/scripts/handlePostSearch.js";
-import handleDeletePost from "./assets/scripts/handleDeletePost.js";
-import handleClearSearch from "./assets/scripts/handleClearSearch.js";
-import handleCloseSearchModal from "./assets/scripts/handleCloseSearchModal.js";
-import handleListPostsInBody from "./assets/scripts/handleListPostsInBody.js";
+import toggleMenu from "./toggleMenu.js";
+import handlePostModal from "./handlePostModal.js";
+import handlePostSearch from "./handlePostSearch.js";
+import handleDeletePost from "./handleDeletePost.js";
+import handleClearSearch from "./handleClearSearch.js";
+import handleCloseSearchModal from "./handleCloseSearchModal.js";
+import handleListPostsInBody from "./handleListPostsInBody.js";
 
 function handleImports() {
   toggleMenu();
